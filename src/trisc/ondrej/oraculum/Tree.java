aade654ce30797b8b886d2b0dd173e065d117cbb
Ondrej Trisc <ondrejtrisc@gmail.com>
2019-10-05 19:03:12 +0200

    constructor for the class Tree created

--- src/trisc/ondrej/oraculum/Tree.java
+++ src/trisc/ondrej/oraculum/Tree.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 
 class Tree {
 
+    static ArrayList<Tree> T;
+
     private int n;
     private ArrayList<Tree> children;
+
+    Tree(ArrayList<Tree> trees) {
+
+        this.n = trees.size();
+        this.children = trees;
+        T.add(this);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
